3ae4ec1459f7b0edd008685dc7e4559fd39592f5
m1dnightgmrofficial <m1dnightgmrofficial@gmail.com>
2026-02-14 07:00:47 +0000

    init 2 ig

--- server/app.js
+++ server/app.js
@@ -1,27 +1,23 @@
-const fastify=require('fastify')({logger:true});
-const path=require('path');
-fastify.register(require('@fastify/static'), {
-  root:path.join(__dirname,'public'),
-  prefix:'/', 
-});
-fastify.register(require('@fastify/cors'), { 
-  origin: '*',
-});
-fastify.get('/',async(request,reply)=>{
-  reply.send({ hello: 'world' })
-});
-fastify.get('/cc',async(request,reply)=>{
-  reply.sendFile('/cc/index.html');
-});
-fastify.listen({
-  port:3000,
-  host: "0.0.0.0"
-},(err,address)=>{
-  if(err){
-    fastify.log.error(err)
-    process.exit(1)
-  }
-  console.log(`Server is now listening on ${address}`);
-  console.log(`  => ${address}/cc`);
-});
-// start();
+'use strict';
+const path=require('node:path');
+const AutoLoad=require('@fastify/autoload');
+const options={};
+module.exports=async function (fastify, opts) {
+  fastify.register(require('@fastify/helmet'),{
+    // contentSecurityPolicy:true,
+    xFrameOptions: "ALLOW-FROM *",
+  });
+  fastify.register(require('@fastify/static'), {
+    root: path.join(__dirname, 'public'),
+    prefix: '/public/',
+  });
+  fastify.register(AutoLoad, {
+    dir: path.join(__dirname, 'plugins'),
+    options: Object.assign({}, opts)
+  });
+  fastify.register(AutoLoad, {
+    dir: path.join(__dirname, 'routes'),
+    options: Object.assign({}, opts)
+  });
+}
+module.exports.options=options;
