4cf5542db20fa5aa0b3c45e540a5d69778fe6c84
m1dnightgmrofficial <m1dnightgmrofficial@gmail.com>
2026-02-18 15:52:38 +0000

    teurdyc

--- server/app.js
+++ server/app.js
@@ -1,23 +1,33 @@
-'use strict';
-const path=require('node:path');
-const AutoLoad=require('@fastify/autoload');
-const options={};
-module.exports=async function (fastify, opts) {
-  fastify.register(require('@fastify/helmet'),{
-    // contentSecurityPolicy:true,
-    xFrameOptions: "ALLOW-FROM *",
-  });
-  fastify.register(require('@fastify/static'), {
-    root: path.join(__dirname, 'public'),
-    prefix: '/public/',
-  });
+'use strict'
+
+const path = require('node:path')
+const AutoLoad = require('@fastify/autoload')
+
+// Pass --options via CLI arguments in command to enable these options.
+const options = {}
+
+module.exports = async function (fastify, opts) {
+  // Place here your custom code!
+
+  // Do not touch the following lines
+
+  // This loads all plugins defined in plugins
+  // those should be support plugins that are reused
+  // through your application
   fastify.register(AutoLoad, {
     dir: path.join(__dirname, 'plugins'),
     options: Object.assign({}, opts)
-  });
+  })
+  fastify.register(require('@fastify/static'), {
+    root: path.join(__dirname, 'public'), // Serve files from the 'public' directory
+    prefix: '/public/', // Optional: serve files under a /public route prefix
+  })
+  // This loads all plugins defined in routes
+  // define your routes in one of these
   fastify.register(AutoLoad, {
     dir: path.join(__dirname, 'routes'),
     options: Object.assign({}, opts)
-  });
+  })
 }
-module.exports.options=options;
+
+module.exports.options = options
